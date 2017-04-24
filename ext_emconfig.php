cc8ad5dd15399ce31d87e9d8bfcb7753f5d84346
sabrisanson <sanson.sabri@gmail.com>
2017-04-24 17:01:15 +0200

    + Ajout du fichier ext_emconfig.php

---
+++ ext_emconfig.php
@@ -0,0 +1,26 @@
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'Theme vertical',
+	'description' => 'Theme avec menu de navigation vertical',
+	'category' => 'fe',
+	'version' => '0.0.1',
+	'state' => 'stable',
+	'uploadfolder' => true,
+	'createDirs' => '',
+	'clearcacheonload' => true,
+	'author' => 'Sabri Sanson',
+	'author_email' => 'sanson.sabri@gmail.com',
+	'author_company' => 'CDG46',
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+			'typo3' => '7.6.2 - 8.99.99'
+		),
+		'conflicts' => 
+		array (
+		),
+		'suggests' => 
+		array (
+		),
+	),
+);
